f23ba06846809f94fc8b0f921c41bbe37f66ef93
haldorj <haldorjh@gmail.com>
2024-10-11 05:45:25 +0200

    Vulkan initialized

--- Zero/src/vk_engine.cpp
+++ Zero/src/vk_engine.cpp
@@ -7,8 +7,14 @@
 
 #include <chrono>
 #include <thread>
+#include <iostream>
 
-constexpr bool bUseValidationLayers = false;
+#ifdef NDEBUG
+    constexpr bool bUseValidationLayers = false;
+#else
+    constexpr bool bUseValidationLayers = true;
+#endif
+
 
 VulkanEngine* loadedEngine = nullptr;
 
@@ -35,13 +41,143 @@ void VulkanEngine::init()
         throw std::runtime_error("Failed to create GLFW window");
     }
 
+    init_vulkan();
+
+    init_swapchain();
+
+    init_commands();
+
+    init_sync_structures();
+
     // everything went fine
     _isInitialized = true;
 }
 
+void VulkanEngine::init_vulkan()
+{
+    // VulkanBootstrap instance builder abstracts the creation of a Vulkan Instance
+    vkb::InstanceBuilder builder;
+
+    // Application creation info, debug features
+    auto inst_ret = builder
+        .set_app_name("Zero")
+        .request_validation_layers(bUseValidationLayers)
+        .use_default_debug_messenger()
+        .require_api_version(1, 3, 0)
+        .build();
+
+    vkb::Instance vkb_inst = inst_ret.value();
+
+    // store instance handle
+    _instance = vkb_inst.instance;
+    _debug_messenger = vkb_inst.debug_messenger;
+
+    // Create the surface
+    if (glfwCreateWindowSurface(_instance, _window, nullptr, &_surface) != VK_SUCCESS)
+        throw std::runtime_error("failed to create window surface!");
+
+    //vulkan 1.3 features
+    VkPhysicalDeviceVulkan13Features features{ .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_3_FEATURES };
+    features.dynamicRendering = true;
+    features.synchronization2 = true;
+
+    //vulkan 1.2 features
+    VkPhysicalDeviceVulkan12Features features12{ .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_2_FEATURES };
+    features12.bufferDeviceAddress = true;
+    features12.descriptorIndexing = true;
+
+    // Use vkbootstrap to select a gpu. 
+    // We want a gpu that can write to the surface and supports vulkan 1.3 with the correct features
+    vkb::PhysicalDeviceSelector selector{ vkb_inst };
+    vkb::PhysicalDevice physicalDevice = selector
+        .set_minimum_version(1, 3)
+        .set_required_features_13(features)
+        .set_required_features_12(features12)
+        .set_surface(_surface)
+        .select()
+        .value();
+
+    //create the final vulkan device
+    vkb::DeviceBuilder deviceBuilder{ physicalDevice };
+
+    vkb::Device vkbDevice = deviceBuilder.build().value();
+
+    // Store the VkDevice handle used in the rest of a vulkan application
+    _device = vkbDevice.device;
+    _physicalDevice = physicalDevice.physical_device;
+
+    VkPhysicalDeviceProperties Properties{};
+    
+    vkGetPhysicalDeviceProperties(_physicalDevice, &Properties);
+    std::cout << "Chosen GPU: " << "\n";
+    std::cout << "\t" << Properties.deviceName << "\n";
+}
+
+void VulkanEngine::init_swapchain()
+{
+    create_swapchain(_windowExtent.width, _windowExtent.height);
+}
+
+void VulkanEngine::init_commands()
+{
+    
+}
+
+void VulkanEngine::init_sync_structures()
+{
+    
+}
+
+void VulkanEngine::create_swapchain(uint32_t width, uint32_t height)
+{
+    vkb::SwapchainBuilder swapchainBuilder(_physicalDevice, _device, _surface);
+
+    _swapchainImageFormat = VK_FORMAT_B8G8R8A8_UNORM;
+
+    // Building the swapchain
+    // The most important detail here is the present mode, which we have set to VK_PRESENT_MODE_FIFO_KHR.
+    // This way we are doing a hard VSync, which will limit the FPS of the entire engine to the refresh-rate of the monitor.
+
+    vkb::Swapchain vkbSwapchain = swapchainBuilder
+        //.use_default_format_selection()
+        .set_desired_format(VkSurfaceFormatKHR{ .format = _swapchainImageFormat, .colorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR })
+        //use vsync present mode
+        .set_desired_present_mode(VK_PRESENT_MODE_FIFO_KHR)
+        .set_desired_extent(width, height)
+        .add_image_usage_flags(VK_IMAGE_USAGE_TRANSFER_DST_BIT)
+        .build()
+        .value();
+
+    _swapchainExtent = vkbSwapchain.extent;
+    //store swapchain and its related images
+    _swapchain = vkbSwapchain.swapchain;
+    _swapchainImages = vkbSwapchain.get_images().value();
+    _swapchainImageViews = vkbSwapchain.get_image_views().value();
+}
+
+void VulkanEngine::destroy_swapchain()
+{
+    vkDestroySwapchainKHR(_device, _swapchain, nullptr);
+
+    // destroy swapchain resources
+    for (int i = 0; i < _swapchainImageViews.size(); i++) {
+
+        vkDestroyImageView(_device, _swapchainImageViews[i], nullptr);
+    }
+}
+
 void VulkanEngine::cleanup()
 {
+    // Components are deleted in the opposite order they were created
     if (_isInitialized) {
+        destroy_swapchain();
+
+        vkDestroySurfaceKHR(_instance, _surface, nullptr);
+        vkDestroyDevice(_device, nullptr);
+
+        vkb::destroy_debug_utils_messenger(_instance, _debug_messenger);
+        vkDestroyInstance(_instance, nullptr);
+
         glfwDestroyWindow(_window);
         glfwTerminate(); // Terminate GLFW
     }
--- Zero/src/vk_engine.h
+++ Zero/src/vk_engine.h
@@ -17,10 +17,31 @@ public:
 	static VulkanEngine& Get();
 
 private:
+	void init_vulkan();
+	void init_swapchain();
+	void init_commands();
+	void init_sync_structures();
+
+	void create_swapchain(uint32_t width, uint32_t height);
+	void destroy_swapchain();
+
 	bool _isInitialized{ false };
 	int _frameNumber{ 0 };
 	bool stop_rendering{ false };
 	VkExtent2D _windowExtent{ 1700 , 900 };
 
 	GLFWwindow* _window;
+
+	VkInstance _instance; // Vulkan API context, used to access drivers.
+	VkDebugUtilsMessengerEXT _debug_messenger; // Vulkan debug output handle
+	VkPhysicalDevice _physicalDevice; // A GPU. Used to query physical GPU details, like features, capabilities, memory size, etc.
+	VkDevice _device; // The “logical” GPU context that you actually execute things on.
+	VkSurfaceKHR _surface; // Window surface.
+
+	VkSwapchainKHR _swapchain;
+	VkFormat _swapchainImageFormat;
+
+	std::vector<VkImage> _swapchainImages;
+	std::vector<VkImageView> _swapchainImageViews;
+	VkExtent2D _swapchainExtent;
 };
